42ef065cc95cd9676e58fedc3b63cd317a1ee036
Alex Trost <alexrtrost@gmail.com>
2021-03-30 22:56:09 -0400

    Remove Map component for demo

--- slices/index.js
+++ slices/index.js
@@ -1,9 +1,6 @@
 export { default as FullWidthText } from "./FullWidthText";
-
 export { default as FullWidthImage } from "./FullWidthImage";
 export { default as TextContent } from "./TextContent";
 export { default as LinkHeading } from "./LinkHeading";
 export { default as GalleryMosaic } from "./GalleryMosaic";
-
-export { default as MenuCategory } from './MenuCategory'
-export { default as LocationMap } from './LocationMap'
+export { default as MenuCategory } from "./MenuCategory";
