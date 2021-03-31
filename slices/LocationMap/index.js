1886725af565843fac9fa5e5259acc3191fcd265
Alex Trost <alexrtrost@gmail.com>
2021-03-31 16:56:31 -0400

    Add Location Map Component

---
+++ slices/LocationMap/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { array, shape } from "prop-types";
+import { RichText } from "prismic-reactjs";
+import MapBox from "../../components/MapBox";
+
+const MySlice = ({ slice }) => (
+  <section className="location-map">
+    <MapBox latLong={slice.primary.location} />
+    <div>
+      <RichText render={slice.primary.title} />
+      <p className="location-map__location">Location</p>
+      <RichText render={slice.primary.address} />
+    </div>
+    <img src={slice.primary.image.url} alt={slice.primary.image.alt} />
+  </section>
+);
+
+MySlice.propTypes = {
+  slice: shape({
+    primary: shape({
+      title: array.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default MySlice;
